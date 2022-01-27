ff1f8048aafa9b95abeacd1bdbb6e25c4ec0e553
Odixiz <odixiz@42l.fr>
2022-01-27 21:25:21 +0100

    Delete assets/svg directory

--- assets/svg/solarpunk.php
+++
@@ -1,6 +0,0 @@
-<svg class="flag" role="img" viewBox="0 0 6 4" aria-labelledby="flag-sp">
-            <title id="flag-sp">Drapeau solarpunk</title>
-            <rect width="6" height="4" fill="#217d40" />
-            <polygon points="0,0 6,0 0,4" fill="#0d3b52" />
-            <circle cx="3" cy="2" r="0.9" stroke-dasharray="0.28" fill="#d9b110" stroke="#d9b110" />
-          </svg>
