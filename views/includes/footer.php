cdec00fc447444c3ce2f156e7a15b805016495ed
Odixiz <odixiz@42l.fr>
2022-01-29 16:06:51 +0100

    Delete footer.php

--- views/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<center>
-        <small>
-          ©&thinsp;2021&#8209;<?php echo $CD_TIME_YEAR ?> <?php echo $CD_NAME_SITE ?>.
-</br>
-          Développé avec <i class="fas fa-heart" style="color: rgb(255, 38, 38)"></i> par <a href="<?php echo $CD_SITE_DEV ?>" target="_blank" class="deco"><?php echo $CD_NAME_DEV ?></a>.
-</br>
-          Basé sur <a href="https://github.com/mielp/home" class="deco">Miel.P</a>.
-        </small>
-</center>
